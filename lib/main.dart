16b4e0931a0259cb641d84e88ceddd5c0d36524b
bs0305020 <bs0305020@naver.com>
2023-03-24 19:49:38 +0900

    ddafds

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MaterialApp(
+    home:MyApp(),debugShowCheckedModeBanner: false,
+    theme: ThemeData(scaffoldBackgroundColor: Color(0xffd1dce6)),)
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  int a=6;
+  var scroll= ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      if(scroll.position.pixels == scroll.position.maxScrollExtent){
+        setState(() {
+          a=a+3;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      appBar: AppBar( backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(icon: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.black,),
+              onPressed: (){},),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body:CustomScrollView( controller: scroll,
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+              children: [
+                Icon(Icons.account_circle,size: 100,),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child:Text('@바다람사',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold),),
+                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Text('22',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        Text('게시물')
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text('94',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        Text('팔로워')
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text('42',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        Text('팔로잉')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+
+
+                Container(width: MediaQuery.of(context).size.width/1.3, height: 40,
+                  child: Row(
+                    children: [
+                      Flexible(child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('태그',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        color: Colors.white,margin:EdgeInsets.all(3),),flex: 3,),
+                      Flexible(child: Container(color: Colors.white,margin:EdgeInsets.all(3),),flex: 7,),
+                    ],
+                  ),),
+                Container(width: MediaQuery.of(context).size.width/1.3, height: 40,
+                  child: Row(
+                    children: [
+                      Flexible(child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('자기소개',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        color: Colors.white,margin:EdgeInsets.all(3),),flex: 3,),
+                      Flexible(child: Container(color: Colors.white,margin:EdgeInsets.all(3),),flex: 7,),
+                    ],
+                  ),),
+                Container(width: MediaQuery.of(context).size.width/1.3, height: 40,
+                  child: Row(
+                    children: [
+                      Flexible(child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('소속단체',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        color: Colors.white,margin:EdgeInsets.all(3),),flex: 3,),
+                      Flexible(child: Container(color: Colors.white,margin:EdgeInsets.all(3),),flex: 7,),
+                    ],
+                  ),),
+                Container(width: MediaQuery.of(context).size.width/1.3, height: 40,
+                  child: Row(
+                    children: [
+                      Flexible(child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('로그 수',style: TextStyle(fontWeight: FontWeight.bold,),),
+                        color: Colors.white,margin:EdgeInsets.all(3),),flex: 3,),
+                      Flexible(child: Container(color: Colors.white,margin:EdgeInsets.all(3),),flex: 7,),
+                    ],
+                  ),),
+                Row(mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      width: MediaQuery.of(context).size.width/3,
+                      height: 30,
+                      margin: EdgeInsets.fromLTRB(10, 20, 0, 10),
+                    ),
+                    Container(
+                      child: Text('조회',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                      margin: EdgeInsets.fromLTRB(10, 20, 25, 10),
+                    )
+                  ],
+                ),
+
+              ],),
+            ),
+            SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                      (c,i) => Container(color : Colors.white,margin: EdgeInsets.all(5),),
+                  childCount: a,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 3 ))
+
+          ],
+
+        )
+
     );
   }
 }
+
+
+
