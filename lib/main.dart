bf232349dce62bbc1b26524c278429a6200712f2
yunzae <dsa05037@naver.com>
2023-07-11 19:53:32 +0900

    feat: signup.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,236 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
-import 'HomePage.dart';
-import 'loginPage.dart';
-import 'prac.dart';
-
-
-void main() async {
-  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
-  WidgetsFlutterBinding.ensureInitialized();
-  KakaoSdk.init(
-    nativeAppKey: '609139b47c7c939fbf9ef4db2e6576ca',
-    javaScriptAppKey: '4065d5a34fc35fc6d5f85fb693b440aa',
-  );
-  print(await AuthApi.instance.hasToken());
-  if(await AuthApi.instance.hasToken()) {
-    runApp(ToHomePage());
-  }
-  else {
-    //runApp(const ToLoginPage());
-    runApp(ToHomePage());
-  }
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class ToLoginPage extends StatelessWidget {
-  const ToLoginPage({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> tags = [];
+
+  TextEditingController tagController = TextEditingController();
+
+  void addTag(String tag) {
+    if (tag.trim().isNotEmpty) {
+      setState(() {
+        tags.add(tag);
+      });
+      tagController.clear();
+    }
+  }
+
+  void removeTag(String tag) {
+    setState(() {
+      tags.remove(tag);
+    });
+  }
+
+  @override
+  void dispose() {
+    tagController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: TextTheme(
-              bodyText2: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-              )
-          )
-      ),
-      home: LoginPage(),
-    );
-  }
-}
-
-class ToHomePage extends StatelessWidget {
-  const ToHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: TextTheme(
-              bodyText2: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-              )
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(30, 70, 0, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '닉네임을',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    '설정해 주세요',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    '바다와 관련된 이름은 어떤가요?',
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                ],
+              ),
+            ),
           ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                        child: Text('닉네임'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: TextField(
+                            maxLength: 12,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              labelText: '닉네임을 입력하세요',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 10, 20, 20),
+                        height: 60,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            '중복확인',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                        child: Text('이전 로그 수'),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 200, 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: '로그 수를 입력하세요',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: Text('태그')),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2.15,
+                          height: 30,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: tags.length,
+                            itemBuilder: (context, index) {
+                              final tag = tags[index];
+                              return Chip(
+                                label: Text(tag),
+                                onDeleted: () => removeTag(tag),
+                                deleteIcon: Icon(Icons.close),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: TextField(
+                            controller: tagController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        height: 60,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (tagController.text.trim().isNotEmpty) {
+                              addTag(tagController.text);
+                            }
+                          },
+                          child: Text(
+                            '추가',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text('태그는 다른사람이 나를 검색할때 사용됩니다.'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text('ex)단체명, 레밸, 동아리, 동호회, 소속센터'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      home: homePage(),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        color: Colors.white,
+        child: Container(
+          margin: EdgeInsets.all(20),
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('가입하기'),
+          ),
+        ),
+      ),
     );
   }
 }
-
-
