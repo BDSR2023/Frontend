3cda5fc407fabdea1fe396a70f14c91c8e6abc93
Friedjelly77 <121471137+Friedjelly77@users.noreply.github.com>
2023-08-10 12:56:08 +0900

    Update logCreationBase.dart

--- lib/logCreationBase.dart
+++ lib/logCreationBase.dart
@@ -13,9 +13,16 @@ import 'logCreationManager.dart';
 
 
 
-//여기서의 변수들은 logCreationManager.dart에 저장됩니다.
+//여기서의(logCreation1~8) 변수들은 logCreationManager.dart에 저장됩니다.
 //provider사용법을 참고해주세요.
 
+//PageView()로 슬라이드 구현.
+//각 logCreation1~8마다 DefaultTextStyle()로 폰트 설정.
+//logCraetion7과 8은 순서가 바뀌어 있는데, 바꿔야 할것 같습니다.
+//logCreation7은 수정필요.(Notion 9주차 회의)
+
+//마지막 부분에 결과창을 붙여주셔야 할 것 같습니다. 
+//(resultPage1~5인지, result_frame을 붙여야 하는지는 준이에게 물어봐야 할 것 같습니다.)
 
 
 //위의 동그랑미 이동 위젯.
@@ -58,8 +65,8 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 }
 
 
+//위의 위젯 불러와서 아래 위젯의 앱바에서 씁니다.
 
-//위의 위젯 불러와서 앱바에서 쓰기.
 class logCreationsBase extends StatefulWidget {
   const logCreationsBase({Key? key}) : super(key: key);
 
@@ -69,8 +76,8 @@ class logCreationsBase extends StatefulWidget {
 
 class _logCreationsBaseState extends State<logCreationsBase> {
   final PageController pageController = PageController();
-  int currentPage = 0;
-  int totalPageCount = 8;
+  int currentPage = 0; //현재 페이지
+  int totalPageCount = 8; //전체 페이지
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +94,7 @@ class _logCreationsBaseState extends State<logCreationsBase> {
               currentPage = index;
             });
           },
-          // 페이지들을 배열로 추가합니다.
+          // 페이지들을 배열로 추가합니다. 페이지를 더 넣고 싶으시면 여기다 넣어주세요. (아마 따로 폰트설정 필요.(DefaultTextStyle()) )
           children: [
             logCreation1(),
             logCreation2(),
